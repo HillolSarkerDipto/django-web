887f631b326e6dcbe28258aefaa24f1d0802c267
HillolSarkerDipto <hillol.dipto@gmail.com>
2021-06-25 00:36:38 +0600

    loop

--- second class/loop.py
+++ second class/loop.py
@@ -3,3 +3,13 @@
 #Non premitive => string, array
 #python
 #integer , float , string, boolean
+
+integer =10
+float = 10.45
+
+string = "this is django course"
+boolean = True
+boolean_ = False
+char = 'a'
+print  ( integer, string, float , boolean , char)
+
