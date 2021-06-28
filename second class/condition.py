0b39ef7a9cfc8b19c2d3e1626bab43f51b77ea8d
HillolSarkerDipto <hillol.dipto@gmail.com>
2021-06-28 08:37:25 +0600

    condition

---
+++ second class/condition.py
@@ -0,0 +1,13 @@
+a=100
+b=20
+
+if 10>100:
+    print("true")
+else:
+    print("False")
+
+if a>b:
+    print("True")
+else:
+    print("False")
+
--- second class/loop.py
+++ second class/loop.py
@@ -16,3 +16,22 @@ arr = numpy.array([1,2,3,4,5])
 print  ( integer, string, float , boolean , char)
 print (type(integer), type(string), type(float), type(boolean_),type(boolean),type(char))
 print(arr)
+
+#loop => for , while
+print(100/2)
+
+for i in range(10):
+    print(i+1)
+
+for i in range(1 ,10):
+    print(100/i)
+
+for i in range(1,10,2):
+    print(i)
+
+i=0
+n=10
+
+while i<n:
+    print(i)
+    i+=2
