d1e89559e9ed12827c17439a83f58106a5b5a63b
HillolSarkerDipto <hillol.dipto@gmail.com>
2021-06-28 08:02:58 +0600

    loop changes

--- second class/loop.py
+++ second class/loop.py
@@ -3,6 +3,7 @@
 #Non premitive => string, array
 #python
 #integer , float , string, boolean
+import numpy
 
 integer =10
 float = 10.45
@@ -11,5 +12,7 @@ string = "this is django course"
 boolean = True
 boolean_ = False
 char = 'a'
+arr = numpy.array([1,2,3,4,5])
 print  ( integer, string, float , boolean , char)
-
+print (type(integer), type(string), type(float), type(boolean_),type(boolean),type(char))
+print(arr)
